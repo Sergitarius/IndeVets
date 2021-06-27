443cb6325882020c4b3e56f0ba30d93a24d8f7ce
Luchko Sergey <learnSS85@gmail.com>
2021-06-27 21:00:21 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -12,8 +12,10 @@
 // 	});
 let menuBtn = document.querySelector('.burger');
 let menu = document.querySelector('.nav__list');
+let headerBtn = document.querySelector('.header__button');
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+	headerBtn.classList.toggle('active');
 })
